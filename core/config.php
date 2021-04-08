c781401f3ca998ae99419f9fcf671428d7dd52ae
Abdumalik Karimov <uzbekistanes2001@gmailcom>
2021-04-08 15:22:07 +0500

    mysqli convert to pdo

--- core/config.php
+++ core/config.php
@@ -1,13 +1,42 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "lalakucatalog";
 
-define(DB_SERVER, 'localhost');
-define(DB_USERNAME, 'root');
-define(DB_PASSWORD, 'root');
-define(DB_NAME, 'lalaku.uz');
-
-$connect = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if(!$connect){
-    echo "DB has not connected to database.".mysql_error();
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE lalakucatalog";
+    $conn->exec($sql);
+    echo "<h5>Database created successfully</h5>";
+    
+} catch(PDOException $e) {
+    // echo $sql . "<br>" . $e->getMessage();
+    header("location: ../index.php");
 }
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql2 = "CREATE TABLE contact (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(50),
+        comment VARCHAR(100) NOT NULL,
+        message_date VARCHAR(20) NOT NULL
+        )";
+    
+    $conn->exec($sql2);
+    echo "<h5>Table created successfully</h5><br>";
+    
+} catch(PDOException $e) {
+    // echo "<br>" . $sql2 . "<br>" . $e->getMessage();
+    header("location: ../index.php");
+}
+
+
+$conn = null;
 ?>
